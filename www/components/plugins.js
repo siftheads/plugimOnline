5ab44ae1d521655d543bb6d8e0e22e40faf7214a
siftheads <pietropietru7@gmail.com>
2020-05-16 06:23:25 +0900

    apk

--- www/components/plugins.js
+++ www/components/plugins.js
@@ -1,2 +1,16 @@
 // This is a JavaScript file
 
+$(document).on("click","alerta",function(){
+function retorno (){
+}
+novigator.notification.alert("minha mensagem",retorno,"Aviso!,","Aceito");
+});
+
+
+
+$(document).on("click","confirm",function(){
+function confirm (bottonIndex){
+novigator.notification.alert(bottonIndex);
+}
+novigator.notification.confirm("escolha A ou B",confirm,"escolho:",['A','B']);
+)};
